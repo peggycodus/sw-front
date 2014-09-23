a7af8e221578e259a86ac1b0f146d5120e6e4000
Peggy Condon & Diego Avalon <student@epicodus.com>
2014-09-23 16:52:44 -0700

    initial commit

---
+++ app/scripts/controllers/navigation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('swFrontApp').controller('NavigationController', function($scope, $location){
+
+  $scope.isActive = function(path){
+
+    return path === $location.path();
+  };
+});
